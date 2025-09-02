b911520b24d15cd1c600fac31ef9652f19de567e
Arsenty Halimovsky <ghost12@yandex.ru>
2025-09-02 10:34:50 +0300

    Instantiate a new project

---
+++ MiniDungeon/MiniDungeon/MainView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  MiniDungeon
+//
+//  Created by Арсентий Халимовский on 02.09.2025.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
---
+++ MiniDungeon/MiniDungeon/MiniDungeonApp.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+
+@main
+struct MiniDungeonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MainView()
+        }
+    }
+}
