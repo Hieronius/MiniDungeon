e8ba72c466923eec69ebfe7b252117d4be268dd3
Arsenty Halimovsky <ghost12@yandex.ru>
2025-09-09 17:50:06 +0300

    Separate BattleView

---
+++ MiniDungeon/MiniDungeon/View/BattleView.swift
@@ -0,0 +1,98 @@
+import SwiftUI
+
+// MARK: - Battle Screen (View)
+
+extension MainView {
+	
+	@ViewBuilder
+	func buildBattle() -> some View {
+		
+		// MARK: UI
+		
+		Spacer()
+		
+		Text(viewModel.gameState.isHeroTurn ? "Hero Turn" : "Enemy Turn")
+		
+		Spacer()
+		
+		HStack {
+			
+			Spacer()
+			
+			VStack {
+				
+				Text("HP - \(viewModel.gameState.hero.heroCurrentHP) / \(viewModel.gameState.hero.heroMaxHP)")
+				Text("MP - \(viewModel.gameState.hero.currentMana) / \(viewModel.gameState.hero.maxMana)")
+				Text("EP - \(viewModel.gameState.hero.currentEnergy) / \(viewModel.gameState.hero.maxEnergy)")
+				Rectangle()
+					.frame(width: 80, height: 80)
+			}
+			
+			Spacer()
+			
+			VStack {
+				
+				Text("HP - \(viewModel.gameState.enemy.enemyCurrentHP) / \(viewModel.gameState.enemy.enemyMaxHP)")
+				Text("MP - \(viewModel.gameState.enemy.currentMana) / \(viewModel.gameState.enemy.maxMana)")
+				Text("EP - \(viewModel.gameState.enemy.currentEnergy) / \(viewModel.gameState.enemy.maxEnergy)")
+				Rectangle()
+					.frame(width: 80, height: 80)
+			}
+			
+			Spacer()
+		}
+		
+		Spacer()
+		
+		// MARK: Actions
+		
+		List {
+			
+			Section(header: Text("Actions")) {
+				
+				Button("Attack") {
+					viewModel.attack()
+				}
+				
+				Button("Fireball") {
+					viewModel.fireball()
+				}
+				
+				Button("Block") {
+					viewModel.block()
+				}
+				
+				Button("Heal") {
+					viewModel.heal()
+				}
+				
+			}
+			
+			Section(header: Text("Utility")) {
+				
+				Button("End Turn") {
+					viewModel.endHeroTurn()
+				}
+				
+				Button("Restore Stats") {
+					viewModel.restoreStats()
+				}
+			}
+			
+			// MARK: Navigation
+			
+			Section(header: Text("Navigation")) {
+				
+				Button("Go To Dungeon") {
+					viewModel.goToDungeon()
+				}
+				Button("Go To Town") {
+					viewModel.goToTown()
+				}
+				Button("Go To Menu") {
+					viewModel.goToMenu()
+				}
+			}
+		}
+	}
+}
