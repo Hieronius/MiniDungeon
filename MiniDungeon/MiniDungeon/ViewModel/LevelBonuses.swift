302f1690dd3041b9b1a48b923b6ba5b8401115da
Arsenty Halimovsky <ghost12@yandex.ru>
2025-10-30 07:29:33 +0300

    Rename level bonuses

--- MiniDungeon/MiniDungeon/ViewModel/LevelBonuses.swift
+++ MiniDungeon/MiniDungeon/ViewModel/LevelBonuses.swift
@@ -12,48 +12,48 @@ extension MainViewModel {
 			
 		// Common
 			
-		case "Flat HP Bonus": gameState.hero.baseMaxHP += 5
-		case "Flat MP Bonus": gameState.hero.baseMaxMP += 5
-		case "Flat Min Damage Bonus": gameState.hero.baseMinDamage += 1
-		case "Flat Max Damage Bonus": gameState.hero.baseMaxDamage += 1
-		case "Flat Spell Power Bonus": gameState.hero.baseSpellPower += 1
+		case "Common HP Bonus": gameState.hero.baseMaxHP += 5
+		case "Common MP Bonus": gameState.hero.baseMaxMP += 5
+		case "Common Min Damage Bonus": gameState.hero.baseMinDamage += 1
+		case "Common Max Damage Bonus": gameState.hero.baseMaxDamage += 1
+		case "Common Spell Power Bonus": gameState.hero.baseSpellPower += 1
 			
 		// Rare
 			
-		case "Great Hit Chance Bonus": gameState.hero.baseHitChance += 1
-		case "Great Crit Chance Bonus": gameState.hero.baseCritChance += 1
-		case "Great Spell Power Bonus": gameState.hero.baseSpellPower += 1
-		case "Great Defence Bonus": gameState.hero.baseDefence += 1
-		case "Great Damage Bonus":
+		case "Rare Hit Chance Bonus": gameState.hero.baseHitChance += 1
+		case "Rare Crit Chance Bonus": gameState.hero.baseCritChance += 1
+		case "Rare Spell Power Bonus": gameState.hero.baseSpellPower += 1
+		case "Rare Defence Bonus": gameState.hero.baseDefence += 1
+		case "Rare Damage Bonus":
 			gameState.hero.baseMinDamage += 1
 			gameState.hero.baseMaxDamage += 1
-		case "Great MP Bonus": gameState.hero.baseMaxMP += 10
-		case "Great HP Bonus": gameState.hero.baseMaxHP += 10
+		case "Rare MP Bonus": gameState.hero.baseMaxMP += 10
+		case "Rare HP Bonus": gameState.hero.baseMaxHP += 10
 			
 		// Epic
 		
-		case "Big Hit Chance Bonus": gameState.hero.baseHitChance += 2
-		case "Big Crit Chance Bonus": gameState.hero.baseCritChance += 2
-		case "Big Spell Power Bonus": gameState.hero.baseSpellPower += 2
-		case "Big Defence Bonus": gameState.hero.baseDefence += 2
-		case "Big Damage Bonus":
+		case "Epic Hit Chance Bonus": gameState.hero.baseHitChance += 2
+		case "Epic Crit Chance Bonus": gameState.hero.baseCritChance += 2
+		case "Epic Spell Power Bonus": gameState.hero.baseSpellPower += 2
+		case "Epic Defence Bonus": gameState.hero.baseDefence += 2
+		case "Epic Damage Bonus":
 			gameState.hero.baseMinDamage += 2
 			gameState.hero.baseMaxDamage += 2
-		case "Big MP Bonus": gameState.hero.baseMaxMP += 15
-		case "Big HP Bonus": gameState.hero.baseMaxHP += 15
+		case "Epic MP Bonus": gameState.hero.baseMaxMP += 15
+		case "Epic HP Bonus": gameState.hero.baseMaxHP += 15
 			
 		// Legendary
 			
-		case "Perfect Energy Bonus": gameState.hero.baseMaxEP += 1
-		case "Perfect Hit Chance Bonus": gameState.hero.baseHitChance += 3
-		case "Perfect Crit Chance Bonus": gameState.hero.baseCritChance += 3
-		case "Perfect Spell Power Bonus": gameState.hero.baseSpellPower += 3
-		case "Perfect Defence Bonus": gameState.hero.baseDefence += 3
-		case "Perfect Damage Bonus":
+		case "Legendary Energy Bonus": gameState.hero.baseMaxEP += 1
+		case "Legendary Hit Chance Bonus": gameState.hero.baseHitChance += 3
+		case "Legendary Crit Chance Bonus": gameState.hero.baseCritChance += 3
+		case "Legendary Spell Power Bonus": gameState.hero.baseSpellPower += 3
+		case "Legendary Defence Bonus": gameState.hero.baseDefence += 3
+		case "Legendary Damage Bonus":
 			gameState.hero.baseMinDamage += 3
 			gameState.hero.baseMaxDamage += 3
-		case "Perfect MP Bonus": gameState.hero.baseMaxMP += 20
-		case "Perfect HP Bonus": gameState.hero.baseMaxHP += 20
+		case "Legendary MP Bonus": gameState.hero.baseMaxMP += 20
+		case "Legendary HP Bonus": gameState.hero.baseMaxHP += 20
 			
 		default: fatalError("Something went wrong")
 			
