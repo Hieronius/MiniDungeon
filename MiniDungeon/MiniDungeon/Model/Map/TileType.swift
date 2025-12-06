3ff567db95cdc781849db974e2264b169a23ffab
Arsenty Halimovsky <ghost12@yandex.ru>
2025-12-06 23:30:21 +0300

    Add Tile Types

--- MiniDungeon/MiniDungeon/Model/Map/TileType.swift
+++ MiniDungeon/MiniDungeon/Model/Map/TileType.swift
@@ -8,7 +8,19 @@ enum TileType {
 
 	/// Rectangle one for small events like quests, riddles, traps or rewards
 	case corridor
+	
+	/// An event to try to defuse the trap
+	case trap
+	
+	/// Event to host Shrine of Recovery (health + mana/some damage/shadow flask charge)
+	case restoration
+	
+	/// Event to disenchant an item (not potion) to dark energy in a specific rate
+	case disenchant
 
 	/// We use it to create black spots on the map matrix so user can't enter them
 	case empty
+	
+	/// Case for a tile with Chest or Loot option
+	case chest
 }
