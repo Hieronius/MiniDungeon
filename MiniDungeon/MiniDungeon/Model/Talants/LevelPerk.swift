c20204d669aef4588c4a1b48fc0ced791e29c0f7
Arsenty Halimovsky <ghost12@yandex.ru>
2026-02-13 06:21:32 +0300

    Create LevelPerk

---
+++ MiniDungeon/MiniDungeon/Model/Talants/LevelPerk.swift
@@ -0,0 +1,27 @@
+/*
+ 
+ File to brainstorm every possible perk to get after slaying level boss and 5th/10th/15th/20th/25th lvl major boss
+ 
+ Include all levels of rarity you can get
+ You can use any talant and create it's common/rare/epic/legendary version
+ 
+ MARK: - List
+ 
+ - add value to block/heal/damage
+ - armor pen to attack
+ - extra crit value for heal/block
+ - reflect damage after block use (overall or for each enemy attack)
+ - add block value after it's use as a min-max damage to next attack
+ - add heal value after it's use as min-max damage to next attack
+ - attacks can heal for 100/50/25% of damage done
+ - attacks can restore mana for 100/50/25% of damage done
+ - each heal (1 max) per fight can increase max health by 1
+ - attack/heal/block buffs hero for specific stat/bonus
+ - attack/heal/block debuffs an enemy for specific stat/bonus
+ - chance to return EP after using heal/attack/block
+ - extra dark energy after use of some abilities or extracting it from enemy
+ - dark energy/gold/exp loot increase via talant
+ - 
+ */
+
+import Foundation
