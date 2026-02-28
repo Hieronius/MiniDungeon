88bc41a1e9132a1de709e859ed445876afeca30b
Arsenty Halimovsky <ghost12@yandex.ru>
2026-02-28 06:02:27 +0300

    Add some Audio files

---
+++ MiniDungeon/MiniDungeon/ViewModel/Audio.swift
@@ -0,0 +1,35 @@
+import Foundation
+
+extension MainViewModel {
+	
+	func playAttackSound(didMissHit: Bool) {
+		
+		if didMissHit {
+			audioManager.playSound(fileName: "hitMissSound2", extensionName: "mp3")
+			print("miss sound")
+			
+		} else {
+			
+			let soundRoll = Int.random(in: 1...3)
+			
+			switch soundRoll {
+				
+			case 1:
+				audioManager.playSound(fileName: "swordSound", extensionName: "mp3")
+				print("case 1 - sword sound")
+				
+			case 2:
+				audioManager.playSound(fileName: "meleeSound", extensionName: "mp3")
+				print("case 2 - melee sound")
+				
+			case 3:
+				audioManager.playSound(fileName: "animalSound", extensionName: "mp3")
+				print("case 3 - animal sound")
+				
+			default:
+				audioManager.playSound(fileName: "swordSound", extensionName: "mp3")
+				print("default case - sword sound")
+			}
+		}
+	}
+}
