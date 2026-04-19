b63a5bed41525a5616b8c6e6b3cfa6aec21ead25
Arsenty Halimovsky <ghost12@yandex.ru>
2026-04-19 08:56:51 +0300

    Update AudioManager

--- MiniDungeon/MiniDungeon/Utility/AudioManager.swift
+++ MiniDungeon/MiniDungeon/Utility/AudioManager.swift
@@ -13,6 +13,20 @@ final class AudioManager {
 	
 	private var player: AVAudioPlayer?
 	
+	private var session: AVAudioSession?
+	
+	init() {
+		
+		do {
+			try AVAudioSession.sharedInstance().setCategory(.ambient, mode: .default)
+			print("AudioSession set to respect silent mode and to be able to mix with other mixable sounds from the device")
+			
+		}
+			catch {
+				print("can't set AudioManager to respect silent mode")
+			}
+	}
+	
 	func playSound(fileName: String, extensionName: String) {
 		
 		guard let url = Bundle.main.url(forResource: fileName, withExtension: extensionName) else {
