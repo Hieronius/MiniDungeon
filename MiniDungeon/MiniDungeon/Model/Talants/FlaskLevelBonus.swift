cf21bc533e515353a92491b1cad2d44698266ee0
Arsenty Halimovsky <ghost12@yandex.ru>
2026-04-21 08:26:02 +0300

    Update FlaskLevelBonus

--- MiniDungeon/MiniDungeon/Model/Talants/FlaskLevelBonus.swift
+++ MiniDungeon/MiniDungeon/Model/Talants/FlaskLevelBonus.swift
@@ -15,13 +15,16 @@ struct FlaskLevelBonus: Identifiable, Hashable, Codable {
 	var id: UUID
 	var name: String
 	var bonusDescription: String
+	var rarity: Rarity
 	
 	init(name: String,
-		 bonusDescription: String
+		 bonusDescription: String,
+		 rarity: Rarity
 	) {
 		self.id = UUID()
 		self.name = name
 		self.bonusDescription = bonusDescription
+		self.rarity = rarity
 	}
 }
 
@@ -36,27 +39,32 @@ struct FlaskLevelBonusManager {
 		
 		FlaskLevelBonus(
 			name: "Common Healing Bonus",
-			bonusDescription: "+5% healing value"
+			bonusDescription: "+5% healing value",
+			rarity: .common
 		),
 		
 		FlaskLevelBonus(
 			name: "Common Damage Bonus",
-			bonusDescription: "+5% damage value"
+			bonusDescription: "+5% damage value",
+			rarity: .common
 		),
 		
 		FlaskLevelBonus(
 			name: "Common CD Reduction Bonus",
-			bonusDescription: "-1 turn to reset flask CD"
+			bonusDescription: "-1 turn to reset flask CD",
+			rarity: .common
 		),
 		
 		FlaskLevelBonus(
 			name: "Common Charge Back Bonus",
-			bonusDescription: "+5% chance to get charge back after use"
+			bonusDescription: "+5% chance to get charge back after use",
+			rarity: .common
 		),
 		
 		FlaskLevelBonus(
 			name: "Common CD Reset Bonus",
-			bonusDescription: "+5% chance to get flask CD reset"
+			bonusDescription: "+5% chance to get flask CD reset",
+			rarity: .common
 		),
 		
 	]
@@ -65,47 +73,56 @@ struct FlaskLevelBonusManager {
 		
 		FlaskLevelBonus(
 			name: "Rare Healing Bonus",
-			bonusDescription: "+10% healing value"
+			bonusDescription: "+10% healing value",
+			rarity: .rare
 		),
 		
 		FlaskLevelBonus(
 			name: "Rare Damage Bonus",
-			bonusDescription: "+10% damage value"
+			bonusDescription: "+10% damage value",
+			rarity: .rare
 		),
 		
 		FlaskLevelBonus(
 			name: "Rare CD Reduction Bonus",
-			bonusDescription: "-2 turns to reset flask CD"
+			bonusDescription: "-2 turns to reset flask CD",
+			rarity: .rare
 		),
 		
 		FlaskLevelBonus(
 			name: "Rare Charge Back Bonus",
-			bonusDescription: "+10% to get flask charge back after use"
+			bonusDescription: "+10% to get flask charge back after use",
+			rarity: .rare
 		),
 		
 		FlaskLevelBonus(
 			name: "Rare CD Reset Bonus",
-			bonusDescription: "+10% to get flask CD reset after use"
+			bonusDescription: "+10% to get flask CD reset after use",
+			rarity: .rare
 		),
 		
 		FlaskLevelBonus(
 			name: "Rare Damage Buff Bonus",
-			bonusDescription: "+1 min and max damage while flask on CD"
+			bonusDescription: "+1 min and max damage while flask on CD",
+			rarity: .rare
 		),
 		
 		FlaskLevelBonus(
 			name: "Rare Armor Buff Bonus",
-			bonusDescription: "+1 armor while flask on CD"
+			bonusDescription: "+1 armor while flask on CD",
+			rarity: .rare
 		),
 		
 		FlaskLevelBonus(
 			name: "Rare Damage Debuff Bonus",
-			bonusDescription: "-1 min and max damage to the target after use"
+			bonusDescription: "-1 min and max damage to the target after use",
+			rarity: .rare
 		),
 		
 		FlaskLevelBonus(
 			name: "Rare Armor Debuff Bonus",
-			bonusDescription: "-1 armor to the target after use"
+			bonusDescription: "-1 armor to the target after use",
+			rarity: .rare
 		)
 		
 	]
@@ -114,52 +131,62 @@ struct FlaskLevelBonusManager {
 		
 		FlaskLevelBonus(
 			name: "Epic Healing Bonus",
-			bonusDescription: "+15% healing value"
+			bonusDescription: "+15% healing value",
+			rarity: .epic
 		),
 		
 		FlaskLevelBonus(
 			name: "Epic Damage Bonus",
-			bonusDescription: "+15% damage value"
+			bonusDescription: "+15% damage value",
+			rarity: .epic
 		),
 		
 		FlaskLevelBonus(
 			name: "Epic CD Reduction Bonus",
-			bonusDescription: "-4 turns to reset flask CD"
+			bonusDescription: "-4 turns to reset flask CD",
+			rarity: .epic
 		),
 		
 		FlaskLevelBonus(
 			name: "Epic Charge Back Bonus",
-			bonusDescription: "+15% to get flask charge back after use"
+			bonusDescription: "+15% to get flask charge back after use",
+			rarity: .epic
 		),
 		
 		FlaskLevelBonus(
 			name: "Epic CD Reset Bonus",
-			bonusDescription: "+15% to get flask CD reset after use"
+			bonusDescription: "+15% to get flask CD reset after use",
+			rarity: .epic
 		),
 		
 		FlaskLevelBonus(
 			name: "Epic Damage Buff Bonus",
-			bonusDescription: "+2 min and max damage while flask on CD"
+			bonusDescription: "+2 min and max damage while flask on CD",
+			rarity: .epic
 		),
 		
 		FlaskLevelBonus(
 			name: "Epic Armor Buff Bonus",
-			bonusDescription: "+2 armor while flask on CD"
+			bonusDescription: "+2 armor while flask on CD",
+			rarity: .epic
 		),
 		
 		FlaskLevelBonus(
 			name: "Epic Damage Debuff Bonus",
-			bonusDescription: "-2 min and max damage to the enemy after use"
+			bonusDescription: "-2 min and max damage to the enemy after use",
+			rarity: .epic
 		),
 		
 		FlaskLevelBonus(
 			name: "Epic Armor Debuff",
-			bonusDescription: "-2 armor to the enemy after use"
+			bonusDescription: "-2 armor to the enemy after use",
+			rarity: .epic
 		),
 		
 		FlaskLevelBonus(
 			name: "Epic Flask Charge Bonus",
-			bonusDescription: "+1 max charges capacity"
+			bonusDescription: "+1 max charges capacity",
+			rarity: .epic
 		)
 		
 	]
@@ -168,52 +195,62 @@ struct FlaskLevelBonusManager {
 		
 		FlaskLevelBonus(
 			name: "Legendary Healing Bonus",
-			bonusDescription: "+20% healing value"
+			bonusDescription: "+20% healing value",
+			rarity: .legendary
 		),
 		
 		FlaskLevelBonus(
 			name: "Legendary Damage Bonus",
-			bonusDescription: "+20% damage value"
+			bonusDescription: "+20% damage value",
+			rarity: .legendary
 		),
 		
 		FlaskLevelBonus(
 			name: "Legendary CD Reduction Bonus",
-			bonusDescription: "-6 turns to reset Flask CD"
+			bonusDescription: "-6 turns to reset Flask CD",
+			rarity: .legendary
 		),
 		
 		FlaskLevelBonus(
 			name: "Legendary Charge Back Bonus",
-			bonusDescription: "+20% to get charge back after use"
+			bonusDescription: "+20% to get charge back after use",
+			rarity: .legendary
 		),
 		
 		FlaskLevelBonus(
 			name: "Legendary CD Reset Bonus",
-			bonusDescription: "+20% to reset flask CD after use"
+			bonusDescription: "+20% to reset flask CD after use",
+			rarity: .legendary
 		),
 		
 		FlaskLevelBonus(
 			name: "Legendary Damage Buff Bonus",
-			bonusDescription: "+3 min and max damage while flask on CD"
+			bonusDescription: "+3 min and max damage while flask on CD",
+			rarity: .legendary
 		),
 		
 		FlaskLevelBonus(
 			name: "Legendary Armor Buff Bonus",
-			bonusDescription: "+3 armor while flask on CD"
+			bonusDescription: "+3 armor while flask on CD",
+			rarity: .legendary
 		),
 		
 		FlaskLevelBonus(
 			name: "Legendary Damage Debuff Bonus",
-			bonusDescription: "-3 min and max damage to the enemy after use"
+			bonusDescription: "-3 min and max damage to the enemy after use",
+			rarity: .legendary
 		),
 		
 		FlaskLevelBonus(
 			name: "Legendary Armor Debuff Bonus",
-			bonusDescription: "-3 armor to the enemy after use"
+			bonusDescription: "-3 armor to the enemy after use",
+			rarity: .legendary
 		),
 		
 		FlaskLevelBonus(
 			name: "Legendary Flask Charge Bonus",
-			bonusDescription: "+2 max flask charges capacity"
+			bonusDescription: "+2 max flask charges capacity",
+			rarity: .legendary
 		),
 		
 		// TODO: Add when the game will be done to avoid making it too complicated
