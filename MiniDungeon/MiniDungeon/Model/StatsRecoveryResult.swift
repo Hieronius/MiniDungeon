81a02738253b5ac882de94cfa4347de001cd2adf
Arsenty Halimovsky <ghost12@yandex.ru>
2026-04-27 10:02:35 +0300

    Update StatsRecoveryResult

--- MiniDungeon/MiniDungeon/Model/StatsRecoveryResult.swift
+++ MiniDungeon/MiniDungeon/Model/StatsRecoveryResult.swift
@@ -1,5 +1,6 @@
 import Foundation
 
+/// We use this entity to restore hp and mana after each dungeon level for money. Take this model before and apply to hero stats after an interaction
 struct StatsRecoveryResult {
 	
 	var newCurrentHPValue: Int
