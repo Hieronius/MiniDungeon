8cedf84ec9f923b0ed02a04726e66ec89506d189
Arsenty Halimovsky <ghost12@yandex.ru>
2026-06-08 08:25:28 +0300

    Update Rewards

--- MiniDungeon/MiniDungeon/View/RewardsView.swift
+++ MiniDungeon/MiniDungeon/View/RewardsView.swift
@@ -9,55 +9,100 @@ extension MainView {
 		
 		List {
 			
-			Section(header: Text("Outcome")) {
+			Section(header: Text(isEnglish() ? "Outcome" : "Итоги")) {
 				
-				if viewModel.gameState.goldLootToDisplay != 0 {
-					Text("Gold: \(viewModel.gameState.goldLootToDisplay)")
-				}
-				
-				if viewModel.gameState.expLootToDisplay != 0 {
-					Text("Experience: \(viewModel.gameState.expLootToDisplay)")
-				}
-				
-				if viewModel.gameState.darkEnergyLootToDisplay != 0 {
-					Text("Dark Energy: \(viewModel.gameState.darkEnergyLootToDisplay)")
-				}
-				
-				if viewModel.gameState.healthPointsLootToDisplay != 0 {
-					Text("Health Points: \(viewModel.gameState.healthPointsLootToDisplay)")
-				}
-				
-				if viewModel.gameState.manaPointsLootToDisplay != 0 {
-					Text("Mana Points: \(viewModel.gameState.manaPointsLootToDisplay)")
+				if isEnglish() {
+					
+					if viewModel.gameState.goldLootToDisplay != 0 {
+						Text("Gold: \(viewModel.gameState.goldLootToDisplay)")
+					}
+					
+					if viewModel.gameState.expLootToDisplay != 0 {
+						Text("Experience: \(viewModel.gameState.expLootToDisplay)")
+					}
+					
+					if viewModel.gameState.darkEnergyLootToDisplay != 0 {
+						Text("Dark Energy: \(viewModel.gameState.darkEnergyLootToDisplay)")
+					}
+					
+					if viewModel.gameState.healthPointsLootToDisplay != 0 {
+						Text("Health Points: \(viewModel.gameState.healthPointsLootToDisplay)")
+					}
+					
+					if viewModel.gameState.manaPointsLootToDisplay != 0 {
+						Text("Mana Points: \(viewModel.gameState.manaPointsLootToDisplay)")
+					}
+					
+				} else {
+					
+					if viewModel.gameState.goldLootToDisplay != 0 {
+						Text("Золото: \(viewModel.gameState.goldLootToDisplay)")
+					}
+					
+					if viewModel.gameState.expLootToDisplay != 0 {
+						Text("Опыт: \(viewModel.gameState.expLootToDisplay)")
+					}
+					
+					if viewModel.gameState.darkEnergyLootToDisplay != 0 {
+						Text("Темная Энергия: \(viewModel.gameState.darkEnergyLootToDisplay)")
+					}
+					
+					if viewModel.gameState.healthPointsLootToDisplay != 0 {
+						Text("Очки Здоровья: \(viewModel.gameState.healthPointsLootToDisplay)")
+					}
+					
+					if viewModel.gameState.manaPointsLootToDisplay != 0 {
+						Text("Очки Маны: \(viewModel.gameState.manaPointsLootToDisplay)")
+					}
 				}
 			}
 			
 			if !viewModel.gameState.lootToDisplay.isEmpty {
 				
-				Section(header: Text("Loot")) {
+				Section(header: Text(isEnglish() ? "Loot" : "Добыча")) {
 					
 					// Test Line
 					ForEach(viewModel.gameState.lootContainerToDisplay.items, id: \.self) { item in
-						Text(item.label)
-							.foregroundStyle(item.rarity.color)
+						
+						if isEnglish() {
+							Text(item.labelEN)
+								.foregroundStyle(item.rarity.color)
+						} else {
+							Text(item.labelRU)
+								.foregroundStyle(item.rarity.color)
+						}
 					}
 					
 					ForEach(viewModel.gameState.lootContainerToDisplay.weapons, id: \.self) {
 						weapon in
-						Text(weapon.label)
-							.foregroundStyle(weapon.rarity.color)
+						
+						if isEnglish() {
+							Text(weapon.labelEN)
+								.foregroundStyle(weapon.rarity.color)
+							
+						} else {
+							Text(weapon.labelRU)
+								.foregroundStyle(weapon.rarity.color)
+						}
 					}
 					
 					ForEach(viewModel.gameState.lootContainerToDisplay.armors, id: \.self) {
 						armor in
-						Text(armor.label)
-							.foregroundStyle(armor.rarity.color)
+						
+						if isEnglish() {
+							Text(armor.labelEN)
+								.foregroundStyle(armor.rarity.color)
+							
+						} else {
+							Text(armor.labelRU)
+								.foregroundStyle(armor.rarity.color)
+						}
 					}
 					
 				}
 			}
 			
-			Button("Got it") {
+			Button(isEnglish() ? "Got it" : "Продолжить") {
 				viewModel.audioManager.playSound(fileName: "click", extensionName: "mp3")
 				viewModel.getRewardsAndCleanTheScreen()
 				itemToDisplay = nil
