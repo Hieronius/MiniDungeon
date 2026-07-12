f2f357794de4ade0e35cdc6132b52b1027b9203b
Arsenty Halimovsky <ghost12@yandex.ru>
2026-07-12 09:17:10 +0300

    Create DamageBoostOutcome

---
+++ MiniDungeon/MiniDungeon/Model/Map/DamageBoostOutcome.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+/// This enum will determine what damage boost hero will get after completing DamageBoostMiniGame
+enum DamageBoostOutcome {
+	
+	case none
+	case small
+	case medium
+}
