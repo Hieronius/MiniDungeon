f8f01ca55810ab7099d1904fe14a6baa01ddeef2
Arsenty Halimovsky <ghost12@yandex.ru>
2026-08-19 11:01:51 +0300

    Update EnemyStatsView

--- MiniDungeon/MiniDungeon/View/EnemyStatsView.swift
+++ MiniDungeon/MiniDungeon/View/EnemyStatsView.swift
@@ -8,23 +8,43 @@ extension MainView {
 		
 		List {
 			
-			Section(header: Text("Enemy Stats")) {
+			Section(header: Text(isEnglish() ? "Enemy Stats" : "О противнике")) {
 				
-				Text("HP: \(viewModel.gameState.enemy.currentHP)/\(viewModel.gameState.enemy.maxHP)")
-				Text("MP: \(viewModel.gameState.enemy.currentMP)/\(viewModel.gameState.enemy.maxMana)")
+				if isEnglish() {
+					
+					Text("Name: \(viewModel.gameState.enemy.name)")
+					
+					Text("HP: \(viewModel.gameState.enemy.currentHP)/\(viewModel.gameState.enemy.maxHP)")
+					Text("MP: \(viewModel.gameState.enemy.currentMP)/\(viewModel.gameState.enemy.maxMana)")
+					
+				} else {
+					
+					Text("Название: \(viewModel.gameState.enemy.name)")
+					
+					Text("Здоровье: \(viewModel.gameState.enemy.currentHP)/\(viewModel.gameState.enemy.maxHP)")
+					Text("Мана: \(viewModel.gameState.enemy.currentMP)/\(viewModel.gameState.enemy.maxMana)")
+				}
 			}
 			
-			Section(header: Text("Combat")) {
+			Section(header: Text(isEnglish() ? "Combat" : "Бой")) {
 				
-				Text("Damage: \(viewModel.gameState.enemy.minDamage) - \(viewModel.gameState.enemy.maxDamage)")
-				Text("Defence: \(viewModel.gameState.enemy.defence)")
+				if isEnglish() {
+					
+					Text("Damage: \(viewModel.gameState.enemy.minDamage) - \(viewModel.gameState.enemy.maxDamage)")
+					Text("Defence: \(viewModel.gameState.enemy.defence)")
+					
+				} else {
+					
+					Text("Урон: \(viewModel.gameState.enemy.minDamage) - \(viewModel.gameState.enemy.maxDamage)")
+					Text("Броня: \(viewModel.gameState.enemy.defence)")
+				}
 			}
 			
 			// MARK: - Navigation
 			
-			Section(header: Text("Navigation")) {
+			Section(header: Text(isEnglish() ? "Navigation" : "Навигация")) {
 
-				Button("Go To Battle") {
+				Button(isEnglish() ? "Go To Battle" : "К битве") {
 					viewModel.audioManager.playSound(fileName: "click", extensionName: "mp3")
 					viewModel.goToBattle()
 				}
