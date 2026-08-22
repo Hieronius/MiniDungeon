82027cc8ab26b2642e35d9a458ac0001faa4b21c
Arsenty Halimovsky <ghost12@yandex.ru>
2026-08-22 12:33:27 +0300

    Update MenuView

--- MiniDungeon/MiniDungeon/View/MenuView.swift
+++ MiniDungeon/MiniDungeon/View/MenuView.swift
@@ -9,87 +9,20 @@ extension MainView {
 		
 		List {
 			
-			Section(header: Text("Menu")) {
+			Section(header: Text(isEnglish() ? "Menu" : "Меню")) {
 				
-				Button("Dungeon") {
-//					viewModel.audioManager.playSound(fileName: "click", extensionName: "mp3")
+				Button(isEnglish() ? "Dungeon" : "Подземелье") {
 					viewModel.goToDungeon()
 				}
 				
-//				Button("Joystick View") {
-//					
-//					viewModel.goToJoystickView()
-//				}
+			}
+			
+			Section(header: Text(isEnglish() ? "Options": "Настройки")) {
 				
-//				Button("CoinFlipMiniGame") {
-//					
-//					viewModel.goToCoinFlipMiniGame()
-//				}
-//				
-//				Button("Level Perks") {
-//					
-//					viewModel.goToLevelPerk()
-//				}
-//				
-//				Button("Test Collision View") {
-//					
-//					viewModel.goToTestCollisionView()
-//				}
-				
-//				Button("Stats Recovery View") {
-//					viewModel.goToStatsRecovery()
-//				}
-//				
-//				Button("Merchant View") {
-//					viewModel.goToMerchant()
-//				}
-				
-				//				Button("Evasion Mini Game") {
-				//					viewModel.goToEvasionMiniGame()
-				//				}
-				//
-//								Button("Build Shadow Mini Game") {
-//									viewModel.goToShadowBallMiniGame()
-//								}
-				
-				//				Button("Build Shadow Flask") {
-				//					viewModel.goToFlask()
-				//				}
-				// MARK: Uncomment to Erase and Insert a new Game State
-				//				Button("Erase Game State") {
-				//					let flask = Flask()
-				//					let hero = Hero(flask: flask)
-				//					let newState = GameState(hero: hero)
-				//					viewModel.gameState = newState
-				//					viewModel.swiftDataManager.saveGameState(newState)
-				//					viewModel.generateMap()
-				//					viewModel.spawnHero()
-				//				}
-				
-				//				Button("Test Chest Lock Picking Mini Game") {
-				//					viewModel.goToChestLockPickingMiniGame()
-				//				}
-				
-				//				Button("Test Combat Mini Game") {
-				//					viewModel.goToCombatMiniGame()
-				//				}
-				
-				//				Button("Test Trap Defusion Mini Game") {
-				//					viewModel.goToTrapDefusionMiniGame()
-				//				}
-				
-				//				Button("Test Shadow Ball Mini Game") {
-				//					viewModel.goToShadowBallMiniGame()
-				//				}
-				
-				//				Button("Test Battle Screen") {
-				//					viewModel.goToBattle()
-				//				}
-				
-//								Button("Test Timeline View") {
-//									viewModel.goToTestTimelineView()
-//								}
-				
+				Button(isEnglish() ? "Start New Game" : "Начать Новую Игру") {
+					viewModel.resetGameStateToDefault()
+					viewModel.audioManager.playSound(fileName: "click", extensionName: "mp3")
+				}
 			}
 		}
 	}
