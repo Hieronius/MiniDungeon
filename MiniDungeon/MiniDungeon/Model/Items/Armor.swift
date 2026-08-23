1353afd6e9ce01c9de68fd4ff5558cbdc9491b60
Arsenty Halimovsky <ghost12@yandex.ru>
2026-08-23 07:24:16 +0300

    Update Armor

--- MiniDungeon/MiniDungeon/Model/Items/Armor.swift
+++ MiniDungeon/MiniDungeon/Model/Items/Armor.swift
@@ -5,10 +5,12 @@ struct Armor: ItemProtocol, Hashable, Codable, Identifiable {
 	//	let id: UUID
 	//	let id = UUID()
 	let id: String
-	var label: String
+	var labelEN: String
+	var labelRU: String
 	var itemType: ItemType
 	var itemLevel: Int
-	var itemDescription: String
+	var itemDescriptionEN: String
+	var itemDescriptionRU: String
 	var defence: Int
 	var healthBonus: Int
 	var manaBonus: Int
@@ -19,10 +21,12 @@ struct Armor: ItemProtocol, Hashable, Codable, Identifiable {
 	var rarity: Rarity
 	var price: Int
 	
-	init(label: String,
+	init(labelEN: String,
+		 labelRU: String,
 		 itemLevel: Int,
 		 itemType: ItemType,
-		 description: String,
+		 itemDescriptionEN: String,
+		 itemDescriptionRU: String,
 		 defence: Int,
 		 healthBonus: Int,
 		 manaBonus: Int,
@@ -34,11 +38,13 @@ struct Armor: ItemProtocol, Hashable, Codable, Identifiable {
 		 price: Int) {
 		
 		//		self.id = UUID()
-		self.id = "\(label)"
-		self.label = label
+		self.id = "\(labelEN)"
+		self.labelEN = labelEN
+		self.labelRU = labelRU
 		self.itemLevel = itemLevel
 		self.itemType = itemType
-		self.itemDescription = description
+		self.itemDescriptionEN = itemDescriptionEN
+		self.itemDescriptionRU = itemDescriptionRU
 		self.defence = defence
 		self.healthBonus = healthBonus
 		self.manaBonus = manaBonus
@@ -58,10 +64,12 @@ struct Armor: ItemProtocol, Hashable, Codable, Identifiable {
 		static let commonArmors: [Armor] = [
 			
 			Armor(
-				label: "Torn cloak",
+				labelEN: "Torn cloak",
+				labelRU: "Разованная туника",
 				itemLevel: 1,
 				itemType: .armor,
-				description: "+5 health, +5 mana",
+				itemDescriptionEN: "+5 health, +5 mana",
+				itemDescriptionRU: "+5 к максимальному уровню здоровья, +5 к максимальному уровню маны",
 				defence: 0,
 				healthBonus: 5,
 				manaBonus: 5,
@@ -74,10 +82,12 @@ struct Armor: ItemProtocol, Hashable, Codable, Identifiable {
 			),
 			
 			Armor(
-				label: "Ripped Lether Armor",
+				labelEN: "Ripped Lether Armor",
+				labelRU: "Надорванный кожаный доспех",
 				itemLevel: 1,
 				itemType: .armor,
-				description: "+1 defence, +2 health, +2 mana",
+				itemDescriptionEN: "+1 defence, +2 health, +2 mana",
+				itemDescriptionRU: "+1 к броне, +2 к максимальному уровню здоровья, +2 к максимальному уровню маны",
 				defence: 1,
 				healthBonus: 2,
 				manaBonus: 2,
@@ -90,10 +100,12 @@ struct Armor: ItemProtocol, Hashable, Codable, Identifiable {
 			),
 			
 			Armor(
-				label: "Old Priest Robe",
+				labelEN: "Old Priest Robe",
+				labelRU: "Старая роба жреца",
 				itemLevel: 1,
 				itemType: .armor,
-				description: "+10 mana",
+				itemDescriptionEN: "+10 mana",
+				itemDescriptionRU: "+10 к максимальному уровню маны",
 				defence: 0,
 				healthBonus: 0,
 				manaBonus: 10,
@@ -106,10 +118,12 @@ struct Armor: ItemProtocol, Hashable, Codable, Identifiable {
 			),
 			
 			Armor(
-				label: "Colorless Mage Tunic",
+				labelEN: "Colorless Mage Tunic",
+				labelRU: "Выцвевшая туника мага",
 				itemLevel: 1,
 				itemType: .armor,
-				description: "+1 spell power, +5 mana",
+				itemDescriptionEN: "+1 spell power, +5 mana",
+				itemDescriptionRU: "+1 к силе заклинаний, +5 к максимальному уровню маны",
 				defence: 0,
 				healthBonus: 0,
 				manaBonus: 5,
@@ -122,10 +136,12 @@ struct Armor: ItemProtocol, Hashable, Codable, Identifiable {
 			),
 			
 			Armor(
-				label: "Blood Stained Knight Cloak",
+				labelEN: "Blood Stained Knight Cloak",
+				labelRU: "Покрытая кровью туника рыцаря",
 				itemLevel: 1,
 				itemType: .armor,
-				description: "+10 health",
+				itemDescriptionEN: "+10 health",
+				itemDescriptionRU: "+10 к максимальному уровню здоровья",
 				defence: 0,
 				healthBonus: 10,
 				manaBonus: 0,
@@ -138,10 +154,12 @@ struct Armor: ItemProtocol, Hashable, Codable, Identifiable {
 			),
 			
 			Armor(
-				label: "Thin Assasin's Cloak ",
+				labelEN: "Thin Assasin's Cloak",
+				labelRU: "Легкая туника ассасина",
 				itemLevel: 1,
 				itemType: .armor,
-				description: "+1% crit chance",
+				itemDescriptionEN: "+1% crit chance",
+				itemDescriptionRU: "+1% к шансу критического эффекта",
 				defence: 0,
 				healthBonus: 0,
 				manaBonus: 0,
@@ -159,10 +177,12 @@ struct Armor: ItemProtocol, Hashable, Codable, Identifiable {
 		static let rareArmors: [Armor] = [
 			
 			Armor(
-				label: "Damaged Plate Armor",
+				labelEN: "Damaged Plate Armor",
+				labelRU: "Поврежденная пластинчатая броня",
 				itemLevel: 2,
 				itemType: .armor,
-				description: "+2 defence, +10 health",
+				itemDescriptionEN: "+2 defence, +10 health",
+				itemDescriptionRU: "+2 к броне, +10 к максимальному уровню здоровья",
 				defence: 2,
 				healthBonus: 10,
 				manaBonus: 0,
@@ -175,10 +195,12 @@ struct Armor: ItemProtocol, Hashable, Codable, Identifiable {
 			),
 			
 			Armor(
-				label: "Boiled Leather Armor",
+				labelEN: "Boiled Leather Armor",
+				labelRU: "Отличная кожаная броня",
 				itemLevel: 2,
 				itemType: .armor,
-				description: "+2 defence, +10 health, +10 mana",
+				itemDescriptionEN: "+2 defence, +10 health, +10 mana",
+				itemDescriptionRU: "+2 к броне, +10 к максимальному уровню здоровья, +10 к максимальному уровню маны",
 				defence: 2,
 				healthBonus: 10,
 				manaBonus: 10,
@@ -191,10 +213,12 @@ struct Armor: ItemProtocol, Hashable, Codable, Identifiable {
 			),
 			
 			Armor(
-				label: "Mage's robe",
+				labelEN: "Mage's robe",
+				labelRU: "Роба мага",
 				itemLevel: 2,
 				itemType: .armor,
-				description: "+1 defence, +10 mana, +1 spell power",
+				itemDescriptionEN: "+1 defence, +10 mana, +1 spell power",
+				itemDescriptionRU: "+1 к броне, +10 к максимальному уровню маны, +10 к максимальному уровню здоровья",
 				defence: 1,
 				healthBonus: 0,
 				manaBonus: 10,
@@ -207,10 +231,12 @@ struct Armor: ItemProtocol, Hashable, Codable, Identifiable {
 			),
 			
 			Armor(
-				label: "Priest's Tunic",
+				labelEN: "Priest's Tunic",
+				labelRU: "Туника жреца",
 				itemLevel: 2,
 				itemType: .armor,
-				description: "+1 defence, + 20 mana",
+				itemDescriptionEN: "+1 defence, +20 mana",
+				itemDescriptionRU: "+1 к броне, +20 к максимальному уровню маны",
 				defence: 1,
 				healthBonus: 0,
 				manaBonus: 20,
@@ -223,10 +249,12 @@ struct Armor: ItemProtocol, Hashable, Codable, Identifiable {
 			),
 			
 			Armor(
-				label: "Squire Jacket",
+				labelEN: "Squire Jacket",
+				labelRU: "Жилет оруженосца",
 				itemLevel: 2,
 				itemType: .armor,
-				description: "+1 defence, +10 health, + 1% hit chance",
+				itemDescriptionEN: "+1 defence, +10 health, +1% hit chance",
+				itemDescriptionRU: "+1 к броне, +10 к здоровью, +1% к шансу критического эффекта",
 				defence: 1,
 				healthBonus: 10,
 				manaBonus: 0,
@@ -239,10 +267,12 @@ struct Armor: ItemProtocol, Hashable, Codable, Identifiable {
 			),
 			
 			Armor(
-				label: "Thief Costume",
+				labelEN: "Thief Costume",
+				labelRU: "Одеяние вора",
 				itemLevel: 2,
 				itemType: .armor,
-				description: "+2% crit chance, +1% hit chance",
+				itemDescriptionEN: "+2% crit chance, +1% hit chance",
+				itemDescriptionRU: "+2% к шансу критического эффекта, +1% к шансу попадения по противнику",
 				defence: 0,
 				healthBonus: 0,
 				manaBonus: 0,
@@ -263,10 +293,12 @@ struct Armor: ItemProtocol, Hashable, Codable, Identifiable {
 			// Start with just adding penalties like: "-1 crit chance, -1 hit chance and so on"
 			
 			Armor(
-				label: "Great Plate Armor",
+				labelEN: "Great Plate Armor",
+				labelRU: "Отличная пластинчатая броня",
 				itemLevel: 3,
 				itemType: .armor,
-				description: "+2 defence, +20 health, +1% hit chance",
+				itemDescriptionEN: "+2 defence, +20 health, +1% hit chance",
+				itemDescriptionRU: "+2 к броне, +20 к максимальному уровню здоровья, +1% к шансу попадения по противнику",
 				defence: 2,
 				healthBonus: 20,
 				manaBonus: 0,
@@ -279,10 +311,12 @@ struct Armor: ItemProtocol, Hashable, Codable, Identifiable {
 			),
 			
 			Armor(
-				label: "Bronze Armor",
+				labelEN: "Bronze Armor",
+				labelRU: "Бронзовая броня",
 				itemLevel: 3,
 				itemType: .armor,
-				description: "+3 Defence, +15 health, +15 mana",
+				itemDescriptionEN: "+3 Defence, +15 health, +15 mana",
+				itemDescriptionRU: "+3 к броне, +15 к максимальному уровню здоровья, +15 к максимальному уровню маны",
 				defence: 3,
 				healthBonus: 15,
 				manaBonus: 15,
@@ -295,10 +329,12 @@ struct Armor: ItemProtocol, Hashable, Codable, Identifiable {
 			),
 			
 			Armor(
-				label: "Corrupted tunic",
+				labelEN: "Corrupted tunic",
+				labelRU: "Проклятая туника",
 				itemLevel: 3,
 				itemType: .armor,
-				description: "+1 defence, +40 mana, -20 health, +5 spell power, -1% crit chance, -1% hit chance",
+				itemDescriptionEN: "+1 defence, +40 mana, -20 health, +5 spell power, -1% crit chance, -1% hit chance",
+				itemDescriptionRU: "+1 к броне, +40 к максимальному уровню маны, -20 к максимальному уровню здоровья, +5 к силе заклинаний, -1% к шансу критического эффекта, -1% к шансу попадения по противнику",
 				defence: 1,
 				healthBonus: -20,
 				manaBonus: 40,
@@ -311,10 +347,12 @@ struct Armor: ItemProtocol, Hashable, Codable, Identifiable {
 			),
 			
 			Armor(
-				label: "Corrupted Composite Armor",
+				labelEN: "Corrupted Composite Armor",
+				labelRU: "Проклятая пластинчатая броня",
 				itemLevel: 3,
 				itemType: .armor,
-				description: "+5 defence, -20 health, -20 mana, -2 spell power, -1% crit chance, -1% hit chance",
+				itemDescriptionEN: "+5 defence, -20 health, -20 mana, -2 spell power, -1% crit chance, -1% hit chance",
+				itemDescriptionRU: "+5 к броне, -20 к максимальному уровню здоровья, -20 к максимальному уровню маны, -1% к шансу критического эффекта, -1% к шансу попадения по противнику",
 				defence: 5,
 				healthBonus: -20,
 				manaBonus: -20,
@@ -327,10 +365,12 @@ struct Armor: ItemProtocol, Hashable, Codable, Identifiable {
 			),
 			
 			Armor(
-				label: "Great Wisard Robe",
+				labelEN: "Great Wisard Robe",
+				labelRU: "Отличная роба волшебника",
 				itemLevel: 3,
 				itemType: .armor,
-				description: "+1 defence, +20 health, +20 mana, +3 spell power, +1% crit chance, +1% hit chance",
+				itemDescriptionEN: "+1 defence, +20 health, +20 mana, +3 spell power, +1% crit chance, +1% hit chance",
+				itemDescriptionRU: "+1 к броне, +20 к максимальному уровню здоровья, +20 к максимальному уровню маны, +3 к силе заклинаний, +1% к шансу критического эффекта, +1% к шансу попадения по противнику",
 				defence: 1,
 				healthBonus: 20,
 				manaBonus: 20,
@@ -343,10 +383,12 @@ struct Armor: ItemProtocol, Hashable, Codable, Identifiable {
 			),
 			
 			Armor(
-				label: "Corrupted Owl Mantle",
+				labelEN: "Corrupted Owl Mantle",
+				labelRU: "Проклятая мантия совы",
 				itemLevel: 3,
 				itemType: .armor,
-				description: "-2 defence, -10 health, -10 mana, +10 spell power, +2% crit chance, +2% hit chance",
+				itemDescriptionEN: "-2 defence, -10 health, -10 mana, +10 spell power, +2% crit chance, +2% hit chance",
+				itemDescriptionRU: "-2 к броне, -10 к максимальному уровню здоровья, -10 к максимальному уровню маны, +10 к силе заклинаний, +2% к шансу критического эффекта, +2% к шансу попасть по противнику",
 				defence: -2,
 				healthBonus: -10,
 				manaBonus: -10,
@@ -359,10 +401,12 @@ struct Armor: ItemProtocol, Hashable, Codable, Identifiable {
 			),
 			
 			Armor(
-				label: "Corrupted Fox Suit",
+				labelEN: "Corrupted Fox Suit",
+				labelRU: "Проклятый костюм лисы",
 				itemLevel: 3,
 				itemType: .armor,
-				description: "+1 defence, +10% crit chance, -20 health, -20 mana, -5 spell power, -5% hit chance",
+				itemDescriptionEN: "+1 defence, +10% crit chance, -20 health, -20 mana, -5 spell power, -5% hit chance",
+				itemDescriptionRU: "+1 к броне, +10% к шансу критического эффекта, -20 к максимальному уровню здоровья, -20 к максимальному уровню маны, -5 к силе заклинаний, -5% к шансу попадения по противнику",
 				defence: 1,
 				healthBonus: -20,
 				manaBonus: -20,
@@ -381,10 +425,12 @@ struct Armor: ItemProtocol, Hashable, Codable, Identifiable {
 		static let legendaryArmors: [Armor] = [
 			
 			Armor(
-				label: "Bone Armor",
+				labelEN: "Bone Armor",
+				labelRU: "Костяной доспех",
 				itemLevel: 4,
 				itemType: .armor,
-				description: "+5 Defence",
+				itemDescriptionEN: "+5 Defence",
+				itemDescriptionRU: "+5 к броне",
 				defence: 5,
 				healthBonus: 25,
 				manaBonus: 25,
@@ -397,10 +443,12 @@ struct Armor: ItemProtocol, Hashable, Codable, Identifiable {
 			),
 			
 			Armor(
-				label: "Death Robe",
+				labelEN: "Death Robe",
+				labelRU: "Роба смертоностности",
 				itemLevel: 4,
 				itemType: .armor,
-				description: "+2 defence, +50 mana, +5 spell power, +2% crit chance, +2% hit chance, -25 health",
+				itemDescriptionEN: "+2 defence, +50 mana, +5 spell power, +2% crit chance, +2% hit chance, -25 health",
+				itemDescriptionRU: "+2 к броне, +50 к максимальному уровню маны, +5 к силе заклинаний, +2% к шансу критического эффекта, +2% к шансу попасть по противнику, -25 к максимальному уровню здоровья",
 				defence: 2,
 				healthBonus: -25,
 				manaBonus: 50,
@@ -413,10 +461,12 @@ struct Armor: ItemProtocol, Hashable, Codable, Identifiable {
 			),
 			
 			Armor(
-				label: "Snake Armor",
+				labelEN: "Snake Armor",
+				labelRU: "Броня змеи",
 				itemLevel: 4,
 				itemType: .armor,
-				description: "+1 ENERGY, +5 spell power, +5% crit chance, +5% hit chance, -5 DEFENCE, -25 health, -25 mana",
+				itemDescriptionEN: "+1 ENERGY, +5 spell power, +5% crit chance, +5% hit chance, -5 DEFENCE, -25 health, -25 mana",
+				itemDescriptionRU: "+1 к ОЧКАМ ДЕЙСТВИЯ, +5 к силе заклинаний, +5 к шансу попадения по противнику, -5 к БРОНЕ, -25 к максимальному запасу здоровья, -25 к максимальному запасу маны",
 				defence: -5,
 				healthBonus: -25,
 				manaBonus: -25,
@@ -429,10 +479,12 @@ struct Armor: ItemProtocol, Hashable, Codable, Identifiable {
 			),
 			
 			Armor(
-				label: "Golden Armor",
+				labelEN: "Golden Armor",
+				labelRU: "Золотой доспех",
 				itemLevel: 4,
 				itemType: .armor,
-				description: "+2 defence, +100 health, +2% hit chance",
+				itemDescriptionEN: "+2 defence, +100 health, +2% hit chance",
+				itemDescriptionRU: "+2 к броне, +100 к здоровью, +2% к шансу попасть по противнику",
 				defence: 2,
 				healthBonus: 100,
 				manaBonus: 0,
@@ -445,10 +497,12 @@ struct Armor: ItemProtocol, Hashable, Codable, Identifiable {
 			),
 			
 			Armor(
-				label: "Faraam Great Armor",
+				labelEN: "Faraam Great Armor",
+				labelRU: "Великий доспех Фараама",
 				itemLevel: 4,
 				itemType: .armor,
-				description: "+10 DEFENCE, +50 health, -50 mana, -1 ENERGY, -5 spell power, -5% crit chance, -5% hit chance",
+				itemDescriptionEN: "+10 DEFENCE, +50 health, -50 mana, -1 ENERGY, -5 spell power, -5% crit chance, -5% hit chance",
+				itemDescriptionRU: "+10 к БРОНЕ, +50 к максимальному уровню здоровья, -50 к максимальному уровню маны, -1 к ОЧКАМ ДЕЙСТВИЯ, -5 к силе заклинаний, -5% к шансу критического эффекта, -5% к шансу попадения по противнику",
 				defence: 10,
 				healthBonus: +50,
 				manaBonus: -50,
