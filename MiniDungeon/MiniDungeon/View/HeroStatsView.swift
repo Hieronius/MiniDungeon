82224c0f8afd3382d42aab1f22f3628201fa615d
Arsenty Halimovsky <ghost12@yandex.ru>
2026-09-02 07:17:47 +0300

    Update HeroStatsView

--- MiniDungeon/MiniDungeon/View/HeroStatsView.swift
+++ MiniDungeon/MiniDungeon/View/HeroStatsView.swift
@@ -10,70 +10,150 @@ extension MainView {
 		
 		List {
 			
+			// MARK: - Dungeon Stats
+			
+			Section(header: Text(isEnglish() ? "Dungeon" : "Подземелье")) {
+				
+				if isEnglish() {
+					
+					Text("Runs: \(viewModel.gameState.runs)")
+					Text("Dungeon Level: \(viewModel.gameState.currentDungeonLevel)")
+					Text("Rooms explored: \(viewModel.countMapRooms().0)/\(viewModel.countMapRooms().1)")
+					Text("Battles won: \(viewModel.gameState.battlesWon)")
+					
+				} else {
+					
+					Text("Забеги: \(viewModel.gameState.runs)")
+					Text("Уровень подземелья: \(viewModel.gameState.currentDungeonLevel)")
+					Text("Комнат открыто: \(viewModel.countMapRooms().0)/\(viewModel.countMapRooms().1)")
+					Text("Битв выиграно: \(viewModel.gameState.battlesWon)")
+				}
+			}
+			
 			// MARK: - Hero Main Stats
 			
-			Section(header: Text("Hero Stats")) {
+			Section(header: Text(isEnglish() ? "Hero Stats" : "Характеристики Героя")) {
 				
-				Text("Runs: \(viewModel.gameState.runs)")
-				Text("LVL: \(viewModel.gameState.hero.heroLevel)")
-				Text("Experience: \(viewModel.gameState.hero.currentXP)/\(viewModel.gameState.hero.maxXP)")
-				//				Text("Specialisation: \(viewModel.gameState.hero.specialisation?.name ?? "")")
-				Text("Health: \(viewModel.gameState.hero.currentHP)/\(viewModel.gameState.hero.maxHP)")
-				Text("Mana: \(viewModel.gameState.hero.currentMana)/\(viewModel.gameState.hero.maxMana)")
-				Text("Energy: \(viewModel.gameState.hero.currentEnergy)/\(viewModel.gameState.hero.maxEnergy)")
+				if isEnglish() {
+					
+					Text("Level: \(viewModel.gameState.hero.heroLevel)")
+					Text("Experience: \(viewModel.gameState.hero.currentXP)/\(viewModel.gameState.hero.maxXP)")
+					Text("Health: \(viewModel.gameState.hero.currentHP)/\(viewModel.gameState.hero.maxHP)")
+					Text("Mana: \(viewModel.gameState.hero.currentMana)/\(viewModel.gameState.hero.maxMana)")
+					Text("Energy: \(viewModel.gameState.hero.currentEnergy)/\(viewModel.gameState.hero.maxEnergy)")
+					
+				} else {
+					
+					Text("Уровень: \(viewModel.gameState.hero.heroLevel)")
+					Text("Опыт: \(viewModel.gameState.hero.currentXP)/\(viewModel.gameState.hero.maxXP)")
+					Text("Здоровье: \(viewModel.gameState.hero.currentHP)/\(viewModel.gameState.hero.maxHP)")
+					Text("Мана: \(viewModel.gameState.hero.currentMana)/\(viewModel.gameState.hero.maxMana)")
+					Text("Очки действия: \(viewModel.gameState.hero.currentEnergy)/\(viewModel.gameState.hero.maxEnergy)")
+				}
 			}
 			
 			// MARK: - Flask Main Stats
 			
-			Section(header: Text("Flask Stats")) {
+			Section(header: Text(isEnglish() ? "Flask Stats" : "Характеристики Фляги")) {
 				
-				Text("LVL: \(viewModel.gameState.hero.flask.level)")
-				Text("Experience: \(viewModel.gameState.hero.flask.currentXP)/\(viewModel.gameState.hero.flask.expToLevelUP)")
-				Text("Flask Charges: \(viewModel.gameState.hero.flask.currentCharges)/\(viewModel.gameState.hero.flask.currentMaxCharges)")
+				if isEnglish() {
+					
+					Text("Level: \(viewModel.gameState.hero.flask.level)")
+					Text("Experience: \(viewModel.gameState.hero.flask.currentXP)/\(viewModel.gameState.hero.flask.expToLevelUP)")
+					Text("Flask Charges: \(viewModel.gameState.hero.flask.currentCharges)/\(viewModel.gameState.hero.flask.currentMaxCharges)")
+					
+				} else {
+					
+					Text("Уровень: \(viewModel.gameState.hero.flask.level)")
+					Text("Опыт: \(viewModel.gameState.hero.flask.currentXP)/\(viewModel.gameState.hero.flask.expToLevelUP)")
+					Text("Заряды: \(viewModel.gameState.hero.flask.currentCharges)/\(viewModel.gameState.hero.flask.currentMaxCharges)")
+				}
 			}
 			
 			// MARK: - Hero Combat Stats
 			
-			Section(header: Text("Combat")) {
-				Text("Damage: \(viewModel.gameState.hero.minDamage) - \(viewModel.gameState.hero.maxDamage)")
-				Text("Defence: \(viewModel.gameState.hero.defence)")
-				Text("Crit Chance: \(viewModel.gameState.hero.critChance)%")
-				Text("Hit Chance: \(viewModel.gameState.hero.hitChance)%")
-				Text("Spell Power: \(viewModel.gameState.hero.spellPower)")
+			Section(header: Text(isEnglish() ? "Combat" : "Бой")) {
+				
+				if isEnglish() {
+					
+					Text("Damage: \(viewModel.gameState.hero.minDamage) - \(viewModel.gameState.hero.maxDamage)")
+					Text("Defence: \(viewModel.gameState.hero.defence)")
+					Text("Crit Chance: \(viewModel.gameState.hero.critChance)%")
+					Text("Hit Chance: \(viewModel.gameState.hero.hitChance)%")
+					Text("Spell Power: \(viewModel.gameState.hero.spellPower)")
+					
+				} else {
+					
+					Text("Урон: \(viewModel.gameState.hero.minDamage) - \(viewModel.gameState.hero.maxDamage)")
+					Text("Броня: \(viewModel.gameState.hero.defence)")
+					Text("Шанс на критический эффект: \(viewModel.gameState.hero.critChance)%")
+					Text("Шанс на попадение по противнику: \(viewModel.gameState.hero.hitChance)%")
+					Text("Сила заклинаний: \(viewModel.gameState.hero.spellPower)")
+				}
 			}
 			
 			// MARK: - Utility
 			
-			Section(header: Text("Utility")) {
-				Text("Gold: \(viewModel.gameState.heroGold)")
-				Text("Current Dark Energy: \(viewModel.gameState.heroDarkEnergy)")
-				Text("Dark Energy Overall: \(viewModel.gameState.heroMaxDarkEnergyOverall)")
+			Section(header: Text(isEnglish() ? "Currency" : "Валюта")) {
+				
+				if isEnglish() {
+					
+					Text("Gold: \(viewModel.gameState.heroGold)")
+					Text("Current Dark Energy: \(viewModel.gameState.heroDarkEnergy)")
+					Text("Dark Energy Overall: \(viewModel.gameState.heroMaxDarkEnergyOverall)")
+					
+				} else {
+					
+					Text("Золото: \(viewModel.gameState.heroGold)")
+					Text("Текущее количество темной энергии: \(viewModel.gameState.heroDarkEnergy)")
+					Text("Получено темной энергии всего: \(viewModel.gameState.heroMaxDarkEnergyOverall)")
+				}
 			}
 			
 			// MARK: - Current Weapon Slot
 			
 			if viewModel.gameState.hero.weaponSlot != nil {
-				Section(header: Text("Equiped Weapon Slot Effects")) {
-					Text("\(viewModel.gameState.hero.weaponSlot!.label): \(viewModel.gameState.hero.weaponSlot!.itemDescription)")
+				Section(header: Text(isEnglish() ? "Equiped Weapon Slot Effects" : "Эффекты экипированного оружия")) {
+					
+					if isEnglish() {
+						
+						Text("\(viewModel.gameState.hero.weaponSlot!.labelEN): \(viewModel.gameState.hero.weaponSlot!.itemDescriptionEN)")
+						
+					} else {
+						
+						Text("\(viewModel.gameState.hero.weaponSlot!.labelRU): \(viewModel.gameState.hero.weaponSlot!.itemDescriptionRU)")
+					}
 				}
 			}
 			
 			// MARK: - Current Armor Slot
 			
 			if viewModel.gameState.hero.armorSlot != nil {
-				Section(header: Text("Equiped Armor Slot Effects")) {
-					Text("\(viewModel.gameState.hero.armorSlot!.label): \(viewModel.gameState.hero.armorSlot!.itemDescription)")
+				Section(header: Text(isEnglish() ? "Equiped Armor Slot Effects" : "Эффекты экипированной брони")) {
+					
+					if isEnglish() {
+						
+						Text("\(viewModel.gameState.hero.armorSlot!.labelEN): \(viewModel.gameState.hero.armorSlot!.itemDescriptionEN)")
+						
+					} else {
+						
+						Text("\(viewModel.gameState.hero.armorSlot!.labelRU): \(viewModel.gameState.hero.armorSlot!.itemDescriptionRU)")
+					}
 				}
 			}
 			
 			// MARK: Used Potions should be put here
 			
 			if !viewModel.gameState.usedPotionsWithPermanentEffects.isEmpty {
-				Section(header: Text("Permanent Potion Effects")) {
+				Section(header: Text(isEnglish() ? "Permanent Potion Effects" : "Постоянные эффекты зелий")) {
 					
 					ForEach(viewModel.gameState.usedPotionsWithPermanentEffects) { effect in
 						
-						Text("\(effect.label): \(effect.itemDescription)")
+						if isEnglish() {
+							Text("\(effect.labelEN): \(effect.itemDescriptionEN)")
+						} else {
+							Text("\(effect.labelRU): \(effect.itemDescriptionRU)")
+						}
 					}
 				}
 			}
@@ -81,12 +161,18 @@ extension MainView {
 			// MARK: - Hero Level Bonuses
 			
 			if !viewModel.gameState.selectedHeroLevelBonuses.isEmpty {
-				Section(header: Text("Active Hero Level Bonuses")) {
+				
+				Section(header: Text(isEnglish() ? "Active Hero Level Bonuses" : "Активные Бонусы Уровня Героя")) {
 					
 					ForEach(viewModel.gameState.selectedHeroLevelBonuses) { bonus in
 						
-						Text("\(bonus.name): \(bonus.bonusDescription)")
-							.foregroundStyle(bonus.rarity.color)
+						if isEnglish() {
+							Text("\(bonus.nameEN): \(bonus.bonusDescriptionEN)")
+								.foregroundStyle(bonus.rarity.color)
+						} else {
+							Text("\(bonus.nameRU): \(bonus.bonusDescriptionRU)")
+								.foregroundStyle(bonus.rarity.color)
+						}
 					}
 				}
 			}
@@ -94,12 +180,18 @@ extension MainView {
 			// MARK: - Level Perks
 			
 			if !viewModel.gameState.selectedLevelPerks.isEmpty {
-				Section(header: Text("Active Level Perks")) {
+				
+				Section(header: Text(isEnglish() ? "Active Level Perks" : "Активные Перки Уровня")) {
 					
 					ForEach(viewModel.gameState.selectedLevelPerks) { perk in
 						
-						Text("\(perk.name): \(perk.perkDescription)")
-							.foregroundStyle(perk.rarity.color)
+						if isEnglish() {
+							Text("\(perk.nameEN): \(perk.perkDescriptionEN)")
+								.foregroundStyle(perk.rarity.color)
+						} else {
+							Text("\(perk.nameRU): \(perk.perkDescriptionRU)")
+								.foregroundStyle(perk.rarity.color)
+						}
 					}
 				}
 			}
@@ -107,50 +199,54 @@ extension MainView {
 			// MARK: - Flask Level Bonuses
 			
 			if !viewModel.gameState.selectedFlaskLevelBonuses.isEmpty {
-				Section(header: Text("Active Flask Level Bonuses")) {
+				
+				Section(header: Text(isEnglish() ? "Active Flask Level Bonuses" : "Активные Таланты Фляги")) {
 					
 					ForEach(viewModel.gameState.selectedFlaskLevelBonuses) { bonus in
 						
-						Text("\(bonus.name): \(bonus.bonusDescription)")
+						if isEnglish() {
+							Text("\(bonus.nameEN): \(bonus.bonusDescriptionEN)")
+						} else {
+							Text("\(bonus.nameRU): \(bonus.bonusDescriptionRU)")
+						}
 					}
 				}
 			}
 			
 			if !viewModel.gameState.upgradedShrines.isEmpty {
-				Section(header: Text("Active Shrines Effects")) {
+				Section(header: Text(isEnglish() ? "Active Shrines Effects" : "Активные Эффекты Алтарей")) {
 					
 					ForEach(viewModel.gameState.upgradedShrines) { shrine in
 						
-						Text("\(shrine.name): \(shrine.shrineDescription)")
+						if isEnglish() {
+							Text("\(shrine.nameEN): \(shrine.shrineDescriptionEN)")
+						} else {
+							Text("\(shrine.nameEN): \(shrine.shrineDescriptionRU)")
+						}
 					}
 				}
 			}
 			
 			if !viewModel.gameState.upgradedFlaskTalants.isEmpty {
-				Section(header: Text("Active Flask Talants Effects")) {
+				Section(header: Text(isEnglish() ? "Active Flask Talants Effects" : "Активные Таланты Фляги")) {
 					
 					ForEach(viewModel.gameState.upgradedFlaskTalants) { talant in
 						
-						Text("\(talant.name): \(talant.flaskTalantDescription)")
+						if isEnglish() {
+							Text("\(talant.nameEN): \(talant.flaskTalantDescriptionEN)")
+						} else {
+							Text("\(talant.nameRU): \(talant.flaskTalantDescriptionRU)")
+						}
 					}
 				}
 			}
 		}
-		.frame(height: 650)
+		.frame(height: 700)
 		
-		// MARK: - Navigation
-		
-		List {
-			
-			Section(header: Text("Navigation")) {
-				
-				Button("Dungeon") {
-					viewModel.audioManager.playSound(fileName: "click", extensionName: "mp3")
-					viewModel.goToDungeon()
-				}
-//				Button("Inventory") {
-//					viewModel.goToInventory()
-//				}
+		.overlay(alignment: .topTrailing) {
+
+			Button(isEnglish() ? "Close" : "Закрыть") {
+				viewModel.goToDungeon()
 			}
 		}
 	}
