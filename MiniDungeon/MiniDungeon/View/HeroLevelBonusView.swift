2fc9ef0e5a2d5670982fad42d8105851ebe951a3
Arsenty Halimovsky <ghost12@yandex.ru>
2026-09-06 09:16:04 +0300

    Update HeroLevelBonus

--- MiniDungeon/MiniDungeon/View/HeroLevelBonusView.swift
+++ MiniDungeon/MiniDungeon/View/HeroLevelBonusView.swift
@@ -11,10 +11,10 @@ extension MainView {
 			
 			List {
 				
-				Section(header: Text("Hero Level Bonus")) {
+				Section(header: Text(isEnglish() ? "Hero Level Bonus" : "Бонус Уровня Героя")) {
 					
 					ForEach(viewModel.gameState.heroLevelBonusesToChoose.compactMap { $0 }) { bonus in
-						Button(bonus.name) {
+						Button(isEnglish() ? bonus.nameEN : bonus.nameRU) {
 							viewModel.gameState.heroLevelBonusToDisplay = bonus
 						}
 						.foregroundStyle(bonus.rarity.color)
@@ -23,14 +23,28 @@ extension MainView {
 				
 				if viewModel.gameState.heroLevelBonusToDisplay != nil {
 					
-					Section(header: Text("Description")) {
-
-						Text("Name - \(viewModel.gameState.heroLevelBonusToDisplay?.name ?? "")")
-							.foregroundStyle(viewModel.gameState.heroLevelBonusToDisplay?.rarity.color ?? .white)
-							.bold()
-						Text("Description:  \(viewModel.gameState.heroLevelBonusToDisplay?.bonusDescription ?? "")")
-						Button("Choose") {
-							viewModel.applyHeroLevelBonus(viewModel.gameState.heroLevelBonusToDisplay)
+					Section(header: Text(isEnglish() ? "Description" : "Описание")) {
+						
+						if isEnglish() {
+							Text("Name - \(viewModel.gameState.heroLevelBonusToDisplay?.nameEN ?? "")")
+								.foregroundStyle(viewModel.gameState.heroLevelBonusToDisplay?.rarity.color ?? .white)
+								.bold()
+						} else {
+							Text("Название - \(viewModel.gameState.heroLevelBonusToDisplay?.nameRU ?? "")")
+								.foregroundStyle(viewModel.gameState.heroLevelBonusToDisplay?.rarity.color ?? .white)
+								.bold()
+						}
+						
+						if isEnglish() {
+							Text("Description: \(viewModel.gameState.heroLevelBonusToDisplay?.bonusDescriptionEN ?? "")")
+							Button("Choose") {
+								viewModel.applyHeroLevelBonus(viewModel.gameState.heroLevelBonusToDisplay)
+							}
+						} else {
+							Text("Описание: \(viewModel.gameState.heroLevelBonusToDisplay?.bonusDescriptionRU ?? "")")
+							Button("Выбрать") {
+								viewModel.applyHeroLevelBonus(viewModel.gameState.heroLevelBonusToDisplay)
+							}
 						}
 					}
 				}
